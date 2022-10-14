84f6be77071c7ba874ba56c986e2978a56d605e3
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2022-10-14 11:36:48 +0200

    Initial  commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+{
+    const tasks = [
+        {
+            content: "nagrać lekcje",
+            done: false,
+        },
+
+        {
+            content: "zjeść pierogi",
+            cone: true,
+        },
+    ];
+
+    const render = () => {
+
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+
+            <li>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+
+    const init = () => {
+
+        render();
+    };
+
+    init();
+
+}
